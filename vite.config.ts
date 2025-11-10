115ffc5db4639380e7e36056a092e42ddb3b42ef
Aman Agarwal <architechinnovate@gmail.com>
2025-11-10 16:29:39 +0530

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,15 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base:'/webpage/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  // server: {
+  //   host: "::",
+  //   port: 8080,
+  // },
+  plugins: [react()],
+  // plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
 }));
